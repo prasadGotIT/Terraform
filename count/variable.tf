6542a1b3740462c4e39cc8a164c5a34c14ad8e0b
prasad sripathi <prasad18218@gmail.com>
2025-10-17 00:53:16 +0530

    terraform

---
+++ count/variable.tf
@@ -0,0 +1,15 @@
+variable "instances" {
+
+    default = ["mongodb","redis","mysql","rabbitmq" ]
+  
+}
+
+variable "zone_id" {
+      default = "Z015504635YTPJNLLKES7"  
+}
+
+variable "domain_name" {
+
+    default = "spdevhops.shop"
+  
+}
